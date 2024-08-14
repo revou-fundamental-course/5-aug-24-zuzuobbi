88da49ab5d92148358ccf52148b51334ada76380
Zulfikar Robbi <zulfikarrobbi@gmail.com>
2024-08-14 22:36:33 +0700

    edit 1

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function calculateBMI() {
+function calculateBMI() 
+{
     var weight = parseFloat(document.getElementById("weight").value);
     var height = parseFloat(document.getElementById("height").value) / 100; // convert height to meters
 
-    if (isNaN(weight) || isNaN(height)) {
+    if (isNaN(weight) || isNaN(height)) 
+    {
         alert("Please enter valid values for weight and height.");
         return;
     }
@@ -11,18 +13,31 @@ function calculateBMI() {
     bmi = bmi.toFixed(1); // round to 1 decimal place
 
     var result = "";
+    var result2 = "";
 
     if (bmi < 18.5) {
-        result = "Kekurangan berat badan";
+        result = "Kekurangan Berat Badan";
     } else if (bmi >= 18.5 && bmi <= 24.9) {
         result = "Normal (ideal)";
     } else if (bmi >= 25 && bmi <= 29.9) {
-        result = "Kelebihan berat badan";
+        result = "Kelebihan Berat Badan";
     } else if (bmi >= 30) {
         result = "Kegemukan (Obesitas)";
     }
 
-    document.getElementById("result").innerHTML = result + "<br>BMI: " + bmi;
+    if (bmi < 18.5) {
+        result2 = "Anda memiliki berat badan kurang";
+    } else if (bmi >= 18.5 && bmi <= 24.9) {
+        result2 = "Anda memiliki berat badan ideal";
+    } else if (bmi >= 25 && bmi <= 29.9) {
+        result2 = "Anda memiliki berat badan berlebih";
+    } else if (bmi >= 30) {
+        result2 = "Body Shaming";
+    }
+    
+    document.getElementById("result").innerHTML = result;
+    document.getElementById("bmi").innerHTML = bmi;
+    document.getElementById("result2").innerHTML = result2;
 }
 
 function resetFields() {
